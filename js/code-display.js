66bae498e7c77cbb3167d6df7894904afca01fc4
Chris Auclair <chris.auclair@gmail.com>
2014-10-23 21:27:35 -0400

    Create jQuery loop for each match

--- js/code-display.js
+++ js/code-display.js
@@ -1,1 +1,12 @@
 /* HTML code display JS */
+$(document).ready(function(){
+
+	// must be the first the JS does or else
+	var $pageSource = $('html');
+
+	// generate code for each example
+	$pageSource.find('.code-display').each(function(){
+		var id = $(this).attr('data-code-id');
+		var $codeBlock = $('#' + id);
+	});
+});
