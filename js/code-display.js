5e72a3ac3cda9b5a444883b5092f051f2f1174e7
Chris Auclair <chris.auclair@gmail.com>
2014-10-23 22:06:46 -0400

    Exclude unwanted code with a class

--- js/code-display.js
+++ js/code-display.js
@@ -6,10 +6,27 @@ $(document).ready(function(){
 
 	// generate code for each example
 	$pageSource.find('.code-display').each(function(){
-		var id = $(this).attr('data-code-id');
-		var language = ($(this).attr('data-language')) ? $(this).attr('data-language') : '';
+		// get data
+		var id = $(this).data('code-id');
+		var language = ($(this).data('language')) ? $(this).data('language') : '';
+		language = ' data-language="' + language + '"';
+		// get codeblock and remove excluded code
 		var $codeBlock = $('#' + id);
-		var $pre = $('<pre><code data-language="' + language + '">' + $codeBlock.html() + '</code></pre>');
+		$codeBlock = excludeCode($codeBlock);
+
+		// create and append code
+		var $pre = $('<code' + language + '">' + $codeBlock.html() + '</code>');
 		$(this).append($pre);
 	});
+
+	// remove code with exclude class
+	function excludeCode($obj) {
+		$obj.find('.code-display-exclude').each(function(){
+			$(this).remove();
+		});
+
+		return $obj;
+	}
+
+
 });
